d481e86e57d08fb7b2530aecd72c296086b4442a
Alexander Marks-Katz <english3000.org@gmail.com>
2017-10-06 10:39:32 -0700

    Started work on 00_tree_node.rb

--- poly-tree-node/lib/00_tree_node.rb
+++ poly-tree-node/lib/00_tree_node.rb
@@ -1,3 +1,38 @@
 class PolyTreeNode
 
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent
+    @parent#.dup
+  end
+
+  def parent=(node)
+    #self.parent.children.delete(self)
+    @parent = node
+    unless node.nil? || node.children.include?(self)
+      node.children << self
+    end
+  end
+
+  def add_child(node)
+    @children << node unless @children.include?(node)
+  end
+
+  def remove_child(node)
+    @children.delete(node)
+  end
+
+
+  def children
+    @children
+  end
+
+  def value
+    @value#.dup
+  end
+
 end
