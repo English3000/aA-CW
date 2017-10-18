48f1c919c2880eb8b145b8f7e8900dd0319a266e
Alexander Marks-Katz <english3000.org@gmail.com>
2017-10-18 13:25:22 -0700

    complete assoc exercises

--- assocs/app/models/course.rb
+++ assocs/app/models/course.rb
@@ -1,2 +1,21 @@
 class Course < ApplicationRecord
+  has_many :enrollments,
+  class_name: :Enrollment,
+  primary_key: :id,
+  foreign_key: :course_id
+
+  has_many :enrolled_students,
+  through: :enrollments,
+  source: :student
+
+  has_many :prerequisite,
+  class_name: :Course,
+  primary_key: :id,
+  foreign_key: :prereq_id
+
+  has_many :instructor,
+  class_name: :User,
+  primary_key: :id,
+  foreign_key: :instructor_id
+
 end
