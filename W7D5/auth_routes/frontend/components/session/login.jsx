608c57f88d00218a42d765cbbb457153d7852c95
Alexander Marks-Katz <english3000.org@gmail.com>
2017-11-17 12:31:06 -0800

    Front-end Auth practice

---
+++ W7D5/auth_routes/frontend/components/session/login.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      password: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInput(type) {
+    return (event) => {
+      this.setState({ [type]: event.target.value });
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.login(this.state)
+      .then( () => this.props.history.push('/chirps') );
+  }
+
+  render() {
+    return (
+      <div className="session-form">
+        <h2>Log In</h2>
+        <form>
+          <label>Username:
+            <input type="text" value={this.state.username} onChange={this.handleInput('username')} />
+          </label>
+          <label>Email:
+            <input type="text" value={this.state.email} onChange={this.handleInput('email')} />
+          </label>
+          <label>Password:
+            <input type="password" value={this.state.password} onChange={this.handleInput('password')} />
+          </label>
+          <button onClick={this.handleSubmit}>Log In</button>
+        </form>
+      </div>
+    );
+  }
+};
+
+export default Login;
