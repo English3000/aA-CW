23d1bd7be8713f9e153ed956013b59cb9e0a1c6e
Alexander <english3000.org@gmail.com>
2018-01-01 18:45:43 -0800

    ring buffer alg.

---
+++ ring-buffer-algorithm/lib/queue_with_max.rb
@@ -0,0 +1,54 @@
+# Implement a queue with #enqueue and #dequeue, as well as a #max API,
+# a method which returns the maximum element still in the queue. This
+# is trivial to do by spending O(n) time upon dequeuing.
+# Can you do it in O(1) amortized? Maybe use an auxiliary storage structure?
+
+# Use your RingBuffer to achieve optimal shifts! Write any additional
+# methods you need.
+
+require_relative 'ring_buffer'
+
+class QueueWithMax
+  attr_accessor :store
+
+  def initialize
+    @store = RingBuffer.new
+    @max = RingBuffer.new
+  end
+
+  def enqueue(val)
+    @store.push(val)
+    if @max.length > 0
+      @max.push(val) if val > @max[@max.length - 1]
+    else
+      @max.push(val)
+    end
+  end
+
+  def dequeue
+    val = @store.shift
+
+    new_max = RingBuffer.new
+    i = 0
+    while i < @max.length
+      new_max.push(@max[i]) if @max[i] > val
+      i += 1
+    end
+
+    @max = new_max
+    val
+  end
+
+  def max
+    if @max.length > 0
+      @max[@max.length - 1]
+    else
+      @max.push(@store[0])
+    end
+  end
+
+  def length
+    @store.length
+  end
+
+end
